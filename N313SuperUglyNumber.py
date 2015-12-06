46ca7c36608f868b1a4feb262bb22f16b12a34a2
Peter Shen <petershen0307@hotmail.com>
2015-12-06 14:08:41 +0800

    [super ugly number] add new test case.

--- N313SuperUglyNumber.py
+++ N313SuperUglyNumber.py
@@ -1,5 +1,29 @@
+import collections
+
 
 def nthSuperUglyNumber(n, primes):
-    if 1 == n:
-        return 1
-    return None
+    total_number_count = 1
+    current_count = 1
+    non_ugly_numbers = set()
+    ugly_numbers = set([1])
+    while total_number_count != n:
+        current_count += 1
+        is_non_ugly_number = False
+        for non_ugly_num in non_ugly_numbers:
+            if current_count % non_ugly_num == 0:  # non ugly number
+                is_non_ugly_number = True
+                non_ugly_numbers.add(current_count)
+                break
+        if not is_non_ugly_number:
+            is_ugly_number = False
+            for prime in primes:
+                if current_count % prime == 0:  # ugly number
+                    is_ugly_number = True
+                    break
+            if is_ugly_number:
+                total_number_count += 1
+                ugly_numbers.add(current_count)
+            else:
+                non_ugly_numbers.add(current_count)
+    print(sorted(ugly_numbers))
+    return current_count
--- N313TestSuperUglyNumber.py
+++ N313TestSuperUglyNumber.py
@@ -1,6 +1,12 @@
 import unittest
 import N313SuperUglyNumber
 
+
 class TestCase(unittest.TestCase):
-    def test_n_is_1(self):
+    def test_function(self):
         self.assertEqual(N313SuperUglyNumber.nthSuperUglyNumber(1, []), 1)
+        # [1, 2, 4, 7, 8, 13, 14, 16, 19, 26, 28, 32]
+        self.assertEqual(N313SuperUglyNumber.nthSuperUglyNumber(12, [2, 7, 13, 19]), 32)
+
+if __name__ == '__main__':
+    unittest.main()
