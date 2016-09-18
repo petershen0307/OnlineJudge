7aee5d99a34ff5fa636582af790e143e0385f811
PeterShen <petershen0307@hotmail.com>
2016-09-18 17:23:25 +0800

    solved problem 217 and 350

--- leetCode/217ContainsDuplicate/go/src/main/main.go
+++ leetCode/217ContainsDuplicate/go/src/main/main.go
@@ -1,5 +1,23 @@
 package main
 
-func containsDuplicate(nums []int) bool {
+import (
+	"fmt"
+	"sort"
+)
 
+func containsDuplicate(nums []int) bool {
+	sort.Ints(nums)
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i] == nums[i+1] {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	fmt.Println(containsDuplicate([]int{}))
+	fmt.Println(containsDuplicate([]int{1, 3, 5, 4, 2}))
+	fmt.Println(containsDuplicate([]int{1, 3, 5, 4, 2, 55, 66, 3}))
+	fmt.Println(containsDuplicate([]int{1, 3, 5, 4, 2, 55, 66, 3, 1}))
 }
--- leetCode/350IntersectionOfTwoArraysII/go/src/main/main.go
+++ leetCode/350IntersectionOfTwoArraysII/go/src/main/main.go
@@ -1,5 +1,35 @@
 package main
 
-func intersect(nums1 []int, nums2 []int) []int {
+import (
+	"fmt"
+)
 
+func intersect(nums1 []int, nums2 []int) []int {
+	nums2Tmp := make([]int, len(nums2))
+	copy(nums2Tmp, nums2)
+	countMap := make(map[int]int)
+	for i := 0; i < len(nums1); i++ {
+		for j := 0; j < len(nums2Tmp); {
+			if nums1[i] == nums2Tmp[j] {
+				countMap[nums1[i]]++
+				nums2Tmp = append(nums2Tmp[:j], nums2Tmp[j+1:]...)
+				break
+			} else {
+				j++
+			}
+		}
+	}
+	var rSlice []int
+	for key, count := range countMap {
+		for i := 0; i < count; i++ {
+			rSlice = append(rSlice, key)
+		}
+	}
+	return rSlice
+}
+
+func main() {
+	fmt.Println("result:", intersect([]int{1, 2, 3, 4, 2, 2}, []int{2, 2, 5, 6, 3, 1}))
+	fmt.Println("result:", intersect([]int{}, []int{2, 2, 5, 6, 3, 1}))
+	fmt.Println("result:", intersect([]int{2, 3, 2, 3, 2}, []int{2, 2, 5, 6, 3, 3}))
 }
