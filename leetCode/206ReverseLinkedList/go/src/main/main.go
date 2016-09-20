7c1870426110b26243f4739a28f684890dd7889d
PeterShen <petershen0307@hotmail.com>
2016-09-21 00:51:27 +0800

    problem solved with iterator, recursive To Be Continued

--- leetCode/206ReverseLinkedList/go/src/main/main.go
+++ leetCode/206ReverseLinkedList/go/src/main/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /**
  * Definition for singly-linked list.
  * type ListNode struct {
@@ -7,6 +9,36 @@ package main
  *     Next *ListNode
  * }
  */
-func reverseList(head *ListNode) *ListNode {
 
+//ListNode ...
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseListIterator(head *ListNode) *ListNode {
+	var reverseHead *ListNode
+	var tmp *ListNode
+	for i := head; i != nil; i = tmp {
+		tmp = i.Next
+		i.Next = reverseHead
+		reverseHead = i
+	}
+	return reverseHead
+}
+
+func print(head *ListNode) {
+	for i := head; nil != i; i = i.Next {
+		fmt.Print(i, " ")
+	}
+	fmt.Println()
+}
+
+func main() {
+	var head *ListNode
+	head = &ListNode{Val: 1}
+	head.Next = &ListNode{Val: 2}
+	head.Next.Next = &ListNode{Val: 3}
+	print(head)
+	print(reverseListIterator(head))
 }
