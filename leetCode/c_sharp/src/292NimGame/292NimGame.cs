e2beaaaefa2faef662e9f9e53e114dd4f45d1f90
PeterShen <petershen0307@hotmail.com>
2016-10-11 23:20:20 +0800

    solved problem 292

---
+++ leetCode/c_sharp/src/292NimGame/292NimGame.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class _292NimGame
+    {
+        static void Main()
+        {
+            _292NimGame t = new _292NimGame();
+            Console.WriteLine(t.CanWinNim(100));
+            Console.ReadKey();
+        }
+
+        public bool CanWinNim(int n)
+        {
+            return n % 4 != 0;
+        }
+    }
+}
