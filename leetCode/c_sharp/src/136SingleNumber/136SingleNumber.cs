74e4ae0eeaac48ae571f0db4cb03c136be2d2f8e
PeterShen <petershen0307@hotmail.com>
2016-10-12 23:21:48 +0800

    resolve 136 using xor operator

---
+++ leetCode/c_sharp/src/136SingleNumber/136SingleNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class _136SingleNumber
+    {
+        static void Main()
+        {
+            int[] nums = {1, 2, 3, 2, 1};
+            _136SingleNumber r = new _136SingleNumber();
+            Console.WriteLine(r.SingleNumber(nums));
+            Console.ReadKey();
+        }
+
+        // A xor A == 0
+        public int SingleNumber(int[] nums)
+        {
+            int result = nums[0];
+            // lamda function: (num, index) => 0 != index
+            // argument: num, index
+            // return: 0 != index
+            foreach (int i in nums.Where((num, index) => 0 != index))
+            {
+                result ^= i;
+            }
+            return result;
+        }
+    }
+}
