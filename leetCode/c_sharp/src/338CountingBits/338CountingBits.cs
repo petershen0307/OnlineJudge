a2a5d08ffac08ab2b0493393a80db6d90a8b5ad3
PeterShen <petershen0307@hotmail.com>
2016-10-12 22:50:35 +0800

    add comment for the logic of solution

--- leetCode/c_sharp/src/338CountingBits/338CountingBits.cs
+++ leetCode/c_sharp/src/338CountingBits/338CountingBits.cs
@@ -24,6 +24,7 @@ namespace LeetCode
         // 指數1~2 value 2 3
         // 指數2~3 value 4 5 6 7
         // 指數3~4 value 8 9 10 11 12 13 14 15
+        // 指數n~n+1 value n-1~n的每個結果加一
         public int[] CountBits(int num)
         {
             int[] result = new int[num+1];
@@ -37,6 +38,7 @@ namespace LeetCode
                 // (1 << e) == 2^e
                 int end = num < ((1 << (e + 1)) - 1) ? num : ((1 << (e + 1)) - 1);
                 int start = (1 << e);
+                // range [2^e, 2^(e+1)-1]
                 for (int i = start; i <= end; i++)
                 {
                     result[i] = result[i - start] + 1;
