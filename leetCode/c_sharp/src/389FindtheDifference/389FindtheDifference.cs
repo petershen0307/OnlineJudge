ce9774a812148183f9605a7624c06104fa977945
PeterShen <petershen0307@hotmail.com>
2016-10-13 23:03:20 +0800

    resolve problem 389 just like problem 136 single number

---
+++ leetCode/c_sharp/src/389FindtheDifference/389FindtheDifference.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class _389FindtheDifference
+    {
+        static void Main()
+        {
+            _389FindtheDifference t = new _389FindtheDifference();
+            Console.WriteLine(t.FindTheDifference2("avb", "bbva"));
+            Console.ReadKey();
+        }
+
+        public char FindTheDifference(string s, string t)
+        {
+            string totalStr = s + t;
+            char result = totalStr[0];
+            foreach (char i in totalStr.Where((num, index) => 0 != index))
+            {
+                result = (char)(result ^ i);
+            }
+            return result;
+        }
+
+        public char FindTheDifference2(string s, string t)
+        {
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+            string totalStr = s + t;
+            foreach (char i in totalStr)
+            {
+                if (!charCount.ContainsKey(i))
+                {
+                    charCount[i] = 0;
+                }
+                charCount[i]++;
+            }
+            char result = '\0';
+            foreach (KeyValuePair<char, int> i in charCount)
+            {
+                if (i.Value % 2 == 1)
+                {
+                    result = i.Key;
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
