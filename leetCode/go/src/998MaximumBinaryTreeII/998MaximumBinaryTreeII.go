7572a9453ed81895a9c39cb2d4d8bbaf0977d3e9
Peter Shen <petershen0307@hotmail.com>
2019-06-17 23:19:22 +0800

    Create 998MaximumBinaryTreeII.go

---
+++ leetCode/go/src/998MaximumBinaryTreeII/998MaximumBinaryTreeII.go
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func insertIntoMaxTree(root *TreeNode, val int) *TreeNode {
+    // s1. bigger than root, the val be the root, all subtree to left node, cause val is the last value
+    // s2. go right child to find val will be the root of subtree, insert subtree to left of val
+    if nil == root {
+        return nil
+    }
+    
+    newRoot := new(TreeNode)
+    if 0 == val {
+        // copy node
+        newRoot.Val = root.Val
+        newRoot.Left = insertIntoMaxTree(root.Left, 0)
+        newRoot.Right = insertIntoMaxTree(root.Right, 0)
+        return newRoot
+    }
+    
+    if val > root.Val {
+        newRoot.Val = val
+        newRoot.Right = nil
+        newRoot.Left = insertIntoMaxTree(root, 0)
+        return newRoot
+    }
+    newRoot.Val = root.Val
+    newRoot.Left = insertIntoMaxTree(root.Left, 0)
+    newRoot.Right = insertIntoMaxTree(root.Right, val)
+    // append val to right
+    if nil == newRoot.Right {
+        newRoot.Right = new(TreeNode)
+        newRoot.Right.Val = val
+        newRoot.Right.Left = nil
+        newRoot.Right.Right = nil        
+    }
+    return newRoot
+}
