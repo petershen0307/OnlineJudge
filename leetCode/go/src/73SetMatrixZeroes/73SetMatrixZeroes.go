122b0f5c859cd38b5c8cc35ae1ec524ff8d5d567
PeterShen <petershen0307@hotmail.com>
2019-06-20 23:11:04 +0800

    add 73 with O(1) space solution

--- leetCode/go/src/73SetMatrixZeroes/73SetMatrixZeroes.go
+++ leetCode/go/src/73SetMatrixZeroes/73SetMatrixZeroes.go
@@ -30,3 +30,56 @@ func setZeroes(matrix [][]int) {
 		}
 	}
 }
+
+func setZeroes2(matrix [][]int) {
+	rowC := len(matrix)
+	if 0 == rowC {
+		return
+	}
+	colC := len(matrix[0])
+	if 0 == colC {
+		return
+	}
+	isFirstRowZero := false
+	isFirstColZero := false
+	for i := 0; i < rowC; i++ {
+		// check first row is zero
+		if 0 == matrix[i][0] {
+			isFirstRowZero = true
+		}
+	}
+	for j := 0; j < colC; j++ {
+		// check first col is zero
+		if 0 == matrix[0][j] {
+			isFirstColZero = true
+		}
+	}
+	for i := 0; i < rowC; i++ {
+		for j := 0; j < colC; j++ {
+			// handle row col except first
+			if 0 == matrix[i][j] && i != 0 && j != 0 {
+				matrix[i][0] = 0
+				matrix[0][j] = 0
+			}
+		}
+	}
+	for i := 1; i < rowC; i++ {
+		for j := 1; j < colC; j++ {
+			// handle row col except first
+			if 0 == matrix[i][0] || 0 == matrix[0][j] {
+				matrix[i][j] = 0
+			}
+		}
+	}
+	// handle first row and col
+	for i := 0; i < rowC; i++ {
+		if isFirstRowZero {
+			matrix[i][0] = 0
+		}
+	}
+	for j := 0; j < colC; j++ {
+		if isFirstColZero {
+			matrix[0][j] = 0
+		}
+	}
+}
