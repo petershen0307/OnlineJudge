810dfd0411cb1636756a81f1ebc077500bd74a7c
Peter Shen <petershen0307@hotmail.com>
2019-06-30 15:51:10 +0800

    Create 344ReverseString.go

---
+++ leetCode/go/src/344ReverseString/344ReverseString.go
@@ -0,0 +1,9 @@
+func reverseString(s []byte)  {
+    for i := 0; i < len(s); i++ {
+        lastOrderIndex := len(s) - i - 1
+        if i > lastOrderIndex {
+            break
+        }
+        s[i], s[lastOrderIndex] = s[lastOrderIndex], s[i]
+    }
+}
