bb87be8596edcf1ce3374026b852d46255347500
PeterShen <petershen0307@hotmail.com>
2022-01-03 22:48:13 +0800

    1010. Pairs of Songs With Total Durations Divisible by 60
    calculate with math

--- leetCode/go/1010PairsOfSongsWithTotalDurationsDivisibleBy60/1010PairsOfSongsWithTotalDurationsDivisibleBy60.go
+++ leetCode/go/1010PairsOfSongsWithTotalDurationsDivisibleBy60/1010PairsOfSongsWithTotalDurationsDivisibleBy60.go
@@ -1,6 +1,6 @@
 package pairsofsongswithtotaldurationsdivisibleby60
 
-func numPairsDivisibleBy60(time []int) int {
+func numPairsDivisibleBy60_2(time []int) int {
 	result := 0
 	remainder := [60]int{}
 	for _, v := range time {
@@ -19,5 +19,21 @@ func numPairsDivisibleBy60(time []int) int {
 			remainder[v%60]--
 		}
 	}
+	return result
+}
+
+func numPairsDivisibleBy60(time []int) int {
+	result := 0
+	remainder := [60]int{}
+	for _, v := range time {
+		remainder[v%60]++
+	}
+	for i := 1; i < 30; i++ {
+		result += remainder[i] * remainder[len(remainder)-i]
+	}
+	// 0
+	result += remainder[0] * (remainder[0] - 1) / 2
+	// 30
+	result += remainder[30] * (remainder[30] - 1) / 2
 	return result
 }
