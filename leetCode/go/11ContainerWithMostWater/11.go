7467030c22b92f6cfcf6988253adc1469e7dc05a
PeterShen <petershen0307@hotmail.com>
2022-04-05 23:17:09 +0800

    enhance 11. Container With Most Water

--- leetCode/go/11ContainerWithMostWater/11.go
+++ leetCode/go/11ContainerWithMostWater/11.go
@@ -1,18 +1,18 @@
 package containerwithmostwater
 
 func maxArea(height []int) int {
-	xCount := len(height) - 1
 	maxValue := 0
-	for begin, end := 0, len(height)-1; xCount > 0; xCount-- {
+	for begin, end := 0, len(height)-1; begin < end; {
 		smallVal := height[end]
+		xDiff := end - begin
 		if height[begin] < height[end] {
 			smallVal = height[begin]
 			begin++
 		} else {
 			end--
 		}
-		if smallVal*xCount > maxValue {
-			maxValue = smallVal * xCount
+		if smallVal*xDiff > maxValue {
+			maxValue = smallVal * xDiff
 		}
 	}
 	return maxValue
